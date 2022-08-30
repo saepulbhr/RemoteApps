d965b1646528b106f2ca4a81c1417ecef2cde474
saepulbhr <bahrisaepul33@gmail.com>
2022-08-30 21:00:36 +0700

    [UDPATE] add router setting speed

--- src/router/RouterSettingGate.jsx
+++ src/router/RouterSettingGate.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 import {
   createStackNavigator,
   CardStyleInterpolators,
-} from '@react-navigation/stack';
-import GateSetting from '../screens/Gate/pengaturan';
+} from "@react-navigation/stack";
+import GateSetting from "../screens/Gate/pengaturan";
+import SettingSpeed from "../screens/SettingSpeed";
 
 const Stack = createStackNavigator();
 
@@ -12,18 +13,28 @@ function RouterSettingGate(props) {
     <Stack.Navigator
       screenOptions={{
         gestureEnabled: true,
-        gestureDirection: 'horizontal',
+        gestureDirection: "horizontal",
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
       }}
     >
       <Stack.Screen
         component={GateSetting}
-        name='setting gate'
+        name="setting gate"
         options={{
           headerShown: false,
-          title: 'Home',
-          headerTintColor: '#fff',
-          headerTitleAlign: 'left',
+          title: "Home",
+          headerTintColor: "#fff",
+          headerTitleAlign: "left",
+        }}
+      />
+
+      <Stack.Screen
+        name="setting speed"
+        component={SettingSpeed}
+        options={{
+          headerShown: false,
+          title: "kecepatan",
+          headerStyle: { elevation: 0, shadowOpacity: 0 },
         }}
       />
     </Stack.Navigator>
