aefc92cba1ab133a72c7a66f262f4361e1192495
saepulbhr <bahrisaepul33@gmail.com>
2022-08-30 21:02:19 +0700

    [UPDATE] ADD wraping context

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import Routers from './src/router';
-import DashboardScreen from './src/screens/Dashboard';
-import StatusBarScreen from './src/screens/StatusBar';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import Routers from "./src/router";
+import DashboardScreen from "./src/screens/Dashboard";
+import StatusBarScreen from "./src/screens/StatusBar";
+import Store from "./src/store/store";
 
 export default function App() {
   return (
     <>
-      <StatusBar translucent backgroundColor='rgba(0,0,0,0)' />
+      <StatusBar translucent backgroundColor="rgba(0,0,0,0)" />
       <StatusBarScreen />
       <PaperProvider>
-        <Routers />
+        <Store>
+          <Routers />
+        </Store>
       </PaperProvider>
       {/* <DashboardScreen /> */}
     </>
@@ -21,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
