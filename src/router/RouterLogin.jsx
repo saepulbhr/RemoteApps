ed58ebb88d30ced6fa5f665929fd80ef47ff736b
saepulbhr <bahrisaepul33@gmail.com>
2022-08-30 20:56:36 +0700

    [ADD] routing login

---
+++ src/router/RouterLogin.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
+import WelcomeScreen from "../screens/Login/components/WelcomeScreen";
+import FormLogin from "../screens/Login/components/FornLogin";
+
+const Stack = createStackNavigator();
+
+function RouterLogin(props) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
+      <Stack.Screen
+        name="welcome screen"
+        component={WelcomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="form login"
+        component={FormLogin}
+        options={{
+          headerShown: false,
+        }}
+      />
+      
+    </Stack.Navigator>
+  );
+}
+
+export default RouterLogin;
