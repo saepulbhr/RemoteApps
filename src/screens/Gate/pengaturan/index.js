a0d2046466b8ae80d087a78f1a144013df1ba4b7
Saepul Bahri <bahrisaepul33@gmail.com>
2022-09-06 16:09:42 +0700

    [UPDATE] import  theme

--- src/screens/Gate/pengaturan/index.js
+++ src/screens/Gate/pengaturan/index.js
@@ -1,30 +1,28 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Divider, List } from "react-native-paper";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Divider, List, useTheme } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 function GateSetting(props) {
   const navigation = useNavigation();
+  const theme = useTheme();
   const menu = [
-    { id: 1, title: "kecapatan", icon: "speedometer", link: "setting speed" },
+    { id: 1, title: 'Kecapatan', icon: 'speedometer', link: 'setting speed' },
   ];
 
   const handlePress = (menu) => navigation.navigate(menu);
   return (
-    <View style={styles.root}>
+    <View style={[styles.root, { backgroundColor: theme.colors.white }]}>
       {menu.map((menu) => (
-        <TouchableOpacity
-          onPress={() => handlePress(menu.link)}
-          key={menu.link}
-        >
+        <TouchableOpacity onPress={() => handlePress(menu.link)} key={menu.id}>
           <View>
             <List.Item
               title={menu.title}
               left={(props) => (
                 <Icon
-                  name="speedometer"
+                  name='speedometer'
                   {...props}
                   size={24}
                   style={{
@@ -45,6 +43,7 @@ const styles = StyleSheet.create({
   root: {
     paddingLeft: 16,
     paddingRight: 16,
+    flex: 1,
   },
 });
 
