52df72f704338576c0f98643c6bfc6d361637c83
Saepul Bahri <bahrisaepul33@gmail.com>
2022-09-06 16:05:36 +0700

    [ADD] Button back

--- src/router/RouterMenuDevic.jsx
+++ src/router/RouterMenuDevic.jsx
@@ -8,8 +8,12 @@ import ScreenOpen from '../screens/Gate/ScreenOpen';
 import SettingSpeed from '../screens/SettingSpeed';
 import LostConnection from '../screens/Errors/LostConnection';
 import RouterSettingGate from './RouterSettingGate';
-import { Button } from 'react-native-paper';
-import GateStatus from '../screens/Gate/GateStatus';
+import ButtonSetting from '../screens/Buttons/ButtonSetting';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import ButtonBack from '../screens/Buttons/ButtonBack';
+import AddDevice from '../screens/AddDevice';
+import FormAddDevice from '../screens/AddDevice/FormAddDevice';
 
 const Stack = createStackNavigator();
 
@@ -21,7 +25,7 @@ function RouterMenuDevic(props) {
         gestureDirection: 'horizontal',
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
       }}
-    > 
+    >
       <Stack.Screen
         component={DashboardScreen}
         name='Home'
@@ -41,19 +45,9 @@ function RouterMenuDevic(props) {
             elevation: 0,
             shadowOpacity: 0,
           },
-          headerRight: () => (
-             <GateStatus />
-          ),
-        }}
-      />
-
-      <Stack.Screen
-        name='setting speed'
-        component={SettingSpeed}
-        options={{
-          headerShown: true,
-          title: 'kecepatan',
-          headerStyle: { elevation: 0, shadowOpacity: 0 },
+          headerTitleAlign: 'center',
+          headerRight: () => <ButtonSetting />,
+          headerLeft: () => <ButtonBack />,
         }}
       />
 
@@ -61,16 +55,42 @@ function RouterMenuDevic(props) {
         name='setting gate'
         component={RouterSettingGate}
         options={{
-          headerShown: true,
+          headerShown: false,
           title: 'pengaturan',
+          headerTitleAlign: 'center',
           headerStyle: { elevation: 0, shadowOpacity: 0 },
+          headerLeft: () => <ButtonBack />,
+        }}
+      />
+
+      <Stack.Screen
+        name='add device'
+        component={AddDevice}
+        options={{
+          headerShown: true,
+          title: 'Tambah Alat',
+          headerTitleAlign: 'center',
+          headerStyle: { elevation: 0, shadowOpacity: 0 },
+          headerLeft: () => <ButtonBack />,
+        }}
+      />
+
+      <Stack.Screen
+        name='form add device'
+        component={FormAddDevice}
+        options={{
+          headerShown: true,
+          title: 'Tambah Alat',
+          headerTitleAlign: 'center',
+          headerStyle: { elevation: 0, shadowOpacity: 0 },
+          headerLeft: () => <ButtonBack />,
         }}
       />
 
       <Stack.Screen
         name='Error 500'
         component={LostConnection}
-        options={{ headerShown: true, title:'Error' }}
+        options={{ headerShown: true, title: 'Error' }}
       />
     </Stack.Navigator>
   );
